534e294b60143757bd2b9659280b1244c01a6d73
Carl LeBlond <openochem@gmail.com>
2025-08-28 13:58:10 -0400

    integrated chemviewer

--- reaction_prediction_api/app.py
+++ reaction_prediction_api/app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 from rdkit import Chem
 from rdkit.Chem import AllChem, Descriptors
 from rdkit.Chem.AllChem import ReactionFromSmarts
 import json
 
 app = Flask(__name__)
+
+
+# Serve static files from the static directory
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+# Serve Kekule.js files specifically
+@app.route('/kekule_libs/<path:filename>')
+def serve_kekule(filename):
+    return send_from_directory('static/kekule_libs', filename)
+
+
 
 # Your reaction library
 reaction_library = {
